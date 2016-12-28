efba9607c53f36020910a39ce4f6209c9fece3e4
Luis Pujols <luispujols@hotmail.com>
2016-12-28 19:44:24 -0400

    ADD 'Created the button listeners and the Map Object in my main activity, now the map updates correctly when i press any button'

--- MapWithMapObjectApp/app/src/main/java/com/nopeia/pujolsluis/mapwithmapobjectapp/MainActivity.java
+++ MapWithMapObjectApp/app/src/main/java/com/nopeia/pujolsluis/mapwithmapobjectapp/MainActivity.java
@@ -2,12 +2,62 @@ package com.nopeia.pujolsluis.mapwithmapobjectapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mapButton = (Button) findViewById(R.id.map_button);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mapReady) googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            }
+        });
+
+        Button satelliteButton = (Button) findViewById(R.id.satellite_button);
+        satelliteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mapReady) googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            }
+        });
+
+        Button hybridButton = (Button) findViewById(R.id.hybrid_button);
+        hybridButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mapReady) googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            }
+        });
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mapReady = true;
+        googleMap = map;
+        LatLng newYork = new LatLng(40.7484, -73.9857);
+        CameraPosition target = CameraPosition.builder().target(newYork).zoom(14).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
+        
     }
 }
