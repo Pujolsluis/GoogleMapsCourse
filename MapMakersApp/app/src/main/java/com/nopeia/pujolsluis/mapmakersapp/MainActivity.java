49e1e557fd3090f4596fce07fbfb25d7b270206b
Luis Pujols <luispujols@hotmail.com>
2016-12-29 14:49:13 -0400

    ADD 'Added the polyline to the map with all the locations, now the map draws a polyline between each location correctly'

--- MapMakersApp/app/src/main/java/com/nopeia/pujolsluis/mapmakersapp/MainActivity.java
+++ MapMakersApp/app/src/main/java/com/nopeia/pujolsluis/mapmakersapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.nopeia.pujolsluis.mapmakersapp;
 
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -8,12 +9,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import static com.nopeia.pujolsluis.mapmakersapp.R.mipmap.hockey;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -27,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     MarkerOptions montlake;
     MarkerOptions kent;
     MarkerOptions showare;
+    PolylineOptions polylineOfHockeyCenters;
 
 
     static final CameraPosition SEATTLE = CameraPosition.builder()
@@ -44,41 +44,52 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        renton = new MarkerOptions()
+//                .position(new LatLng(47.489805, -122.120502))
+//                .title("Renton")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
+//
+//        kirkland = new MarkerOptions()
+//                .position(new LatLng(47.7301986, -122.1768858))
+//                .title("Kirkland")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
+//
+//        everett = new MarkerOptions()
+//                .position(new LatLng(47.978748, -122.202001))
+//                .title("Everett")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
+//
+//        lynnwood = new MarkerOptions()
+//                .position(new LatLng(47.819533, -122.32288))
+//                .title("Lynnwood")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
+//
+//        montlake = new MarkerOptions()
+//                .position(new LatLng(47.7973733, -122.3281771))
+//                .title("Montlake Terrace")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
+//
+//        kent = new MarkerOptions()
+//                .position(new LatLng(47.385938, -122.258212))
+//                .title("Kent Valley")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
+//
+//        showare = new MarkerOptions()
+//                .position(new LatLng(47.38702, -122.23986))
+//                .title("Showare Center")
+//                .icon(BitmapDescriptorFactory.fromResource(hockey));
 
-        renton = new MarkerOptions()
-                .position(new LatLng(47.489805, -122.120502))
-                .title("Renton")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
-
-        kirkland = new MarkerOptions()
-                .position(new LatLng(47.7301986, -122.1768858))
-                .title("Kirkland")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
-
-        everett = new MarkerOptions()
-                .position(new LatLng(47.978748, -122.202001))
-                .title("Everett")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
-
-        lynnwood = new MarkerOptions()
-                .position(new LatLng(47.819533, -122.32288))
-                .title("Lynnwood")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
-
-        montlake = new MarkerOptions()
-                .position(new LatLng(47.7973733, -122.3281771))
-                .title("Montlake Terrace")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
-
-        kent = new MarkerOptions()
-                .position(new LatLng(47.385938, -122.258212))
-                .title("Kent Valley")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
-
-        showare = new MarkerOptions()
-                .position(new LatLng(47.38702, -122.23986))
-                .title("Showare Center")
-                .icon(BitmapDescriptorFactory.fromResource(hockey));
+          polylineOfHockeyCenters = new PolylineOptions().geodesic(true)
+                  .add(new LatLng(47.489805, -122.120502))
+                  .add(new LatLng(47.7301986, -122.1768858))
+                  .add(new LatLng(47.978748, -122.202001))
+                  .add(new LatLng(47.819533, -122.32288))
+                  .add(new LatLng(47.7973733, -122.3281771))
+                  .add(new LatLng(47.385938, -122.258212))
+                  .add(new LatLng(47.38702, -122.23986))
+                  .add(new LatLng(47.489805, -122.120502))
+                  .color(Color.RED);
 
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
@@ -89,13 +100,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap map) {
         googleMap = map;
         mapReady = true;
-        googleMap.addMarker(renton);
-        googleMap.addMarker(kirkland);
-        googleMap.addMarker(everett);
-        googleMap.addMarker(lynnwood);
-        googleMap.addMarker(montlake);
-        googleMap.addMarker(kent);
-        googleMap.addMarker(showare);
+//        googleMap.addMarker(renton);
+//        googleMap.addMarker(kirkland);
+//        googleMap.addMarker(everett);
+//        googleMap.addMarker(lynnwood);
+//        googleMap.addMarker(montlake);
+//        googleMap.addMarker(kent);
+//        googleMap.addMarker(showare);
+        googleMap.addPolyline(polylineOfHockeyCenters);
         flyTo(SEATTLE);
 
     }
