94e05d5b1439d38ee87cd6ac3d5bfb8ef6359bba
Luis Pujols <luispujols@hotmail.com>
2016-12-29 00:18:05 -0400

    UPDATE 'Added the fragment for the map and configured the API Key, now the map displays correctly in the app'

--- MapMakersApp/app/src/main/java/com/nopeia/pujolsluis/mapmakersapp/MainActivity.java
+++ MapMakersApp/app/src/main/java/com/nopeia/pujolsluis/mapmakersapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.nopeia.pujolsluis.mapmakersapp;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap googleMap;
+    boolean mapReady = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        mapReady = true;
+        LatLng newYork = new LatLng(40.7484, -73.9857);
+        CameraPosition target = CameraPosition.builder().target(newYork).zoom(17).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
 }
